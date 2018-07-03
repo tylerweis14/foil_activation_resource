8b5579855b88252a982a98904201ec5e01e755c8
tylerweis14 <tylerweis14@ksu.edu>
2018-07-03 11:30:13 -0500

    wand changes

--- source/wand.py
+++ source/wand.py
@@ -139,82 +139,44 @@ if 3 in cases:
 #                               rhodium
 ###############################################################################
 if 4 in cases:
-    print('\n')
-    name = 'rhodium'
-    print(name.capitalize())
-    mat = 'Rh'
-    cd_cov = False
-    masses = np.array([0.7, 0.55, 0.5, 0.55])  # mg
-    t_i = 60  # s
-    t_w = 3600*1 + 60  # s
-    counting_time = 600
-    t_f = 3600*2  # s
-    P = 100  # kW(th)
-
-    removal_activity = 0
-    counting_activities = np.zeros(4)
-    for i, m in enumerate(masses):
-        t_ci = t_w + (i * counting_time)
-        t_cf = t_w + ((i + 1) * counting_time)
-        counts, act_rem, act_count = activity_calc(foils[mat], m, P, t_i, t_ci, t_cf, t_f,
-                                                   cd_cov, plotname='plot/{}{}_activity.png'.format(mat.lower(), i + 1))
-        removal_activity += act_rem
-        counting_activities[i] = act_count
-    print('Removal Activity:  {:4.2e}  uCi'.format(removal_activity))
-    write_single_foil(name, P, t_i, t_w-t_i, counting_time, removal_activity, counting_activities, masses, mat)
-
+    wand = Wand()
+    wand.name = 'rhodium'
+    wand.mat = 'Rh'
+    wand.cd = False
+    wand.masses = np.array([0.7, 0.55, .5, .55])  # mg
+    wand.t_i = 60  # s
+    wand.t_w = 3600*1 # s
+    wand.counting_time = 600
+    wand.P = 100  # kW(th)
+    wand.irradiate()
+    
 ###############################################################################
 #                              aluminum
 ###############################################################################
 if 5 in cases:
-    print('\n')
-    name = 'aluminum'
-    print(name.capitalize())
-    mat = 'Al'
-    cd_cov = False
-    masses = np.array([0.3, 0.2, 0.1, 0.2])  # mg
-    t_i = 3600  # s
-    t_w = 3600*66  # s
-    counting_time = 3600
-    t_f = 3600*72  # s
-    P = 100  # kW(th)
-
-    removal_activity = 0
-    counting_activities = np.zeros(4)
-    for i, m in enumerate(masses):
-        t_ci = t_w + (i * counting_time)
-        t_cf = t_w + ((i + 1) * counting_time)
-        counts, act_rem, act_count = activity_calc(foils[mat], m, P, t_i, t_ci, t_cf, t_f,
-                                                   cd_cov, plotname='plot/{}{}_activity.png'.format(mat.lower(), i + 1))
-        removal_activity += act_rem
-        counting_activities[i] = act_count
-    print('Removal Activity:  {:4.2e}  uCi'.format(removal_activity))
-    write_single_foil(name, P, t_i, t_w-t_i, counting_time, removal_activity, counting_activities, masses, mat)
+    wand = Wand()
+    wand.name = 'aluminium'
+    wand.mat = 'Al'
+    wand.cd = False
+    wand.masses = np.array([0.3, 0.2, 0.1, 0.2])  # mg
+    wand.t_i = 3600  # s
+    wand.t_w = 3600*66  # s
+    wand.counting_time = 3600
+    wand.P = 100  # kW(th)
+    wand.irradiate()
 
 ###############################################################################
 #                              uranium
 ###############################################################################
-if 6 in cases:
-    print('\n')
-    name = 'uranium'
-    print(name.capitalize)
-    mat = 'U'
-    cd_cov = False
-    masses = np.array([5.0, 4.35, 4.30, 4.37])  # assumed the same as au for now
-    t_i = 600  # s
-    t_w = 5600  # s
-    counting_time = 600
-    t_f = 10000  # s
-    P = 100  # kW(th)
 
-    removal_activity = 0
-    counting_activities = np.zeros(4)
-    for i, m in enumerate(masses):
-        t_ci = t_w + (i * counting_time)
-        t_cf = t_w + ((i + 1) * counting_time)
-        counts, act_rem, act_count = activity_calc(foils[mat], m, P, t_i, t_ci, t_cf, t_f,
-                                                   cd_cov, plotname='plot/{}{}_activity.png'.format(mat.lower(), i + 1))
-        removal_activity += act_rem
-        counting_activities[i] = act_count
-    print('Removal Activity:  {:4.2e}  uCi'.format(removal_activity))
-    write_single_foil(name, P, t_i, t_w-t_i, counting_time, removal_activity, counting_activities, masses, mat)
+if 6 in cases:
+    wand = Wand()
+    wand.name = 'uranium'
+    wand.mat = 'U'
+    wand.cd = False
+    wand.masses = np.array([5.0, 4.35, 4.30, 4.37])  # mg
+    wand.t_i = 600  # s
+    wand.t_w = 5600  # s
+    wand.counting_time = 600
+    wand.P = 100  # kW(th)
+    wand.irradiate()
