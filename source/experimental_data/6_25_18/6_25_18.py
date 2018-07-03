539187d36094375f9db7bd19be412b1ceda4055e
John Boyington <mjcb@ksu.edu>
2018-07-03 10:13:34 -0500

    started storing experimental data

---
+++ source/experimental_data/6_25_18/6_25_18.py
@@ -0,0 +1,13 @@
+# storing the data from the 6_25_18 mpfd irradiation
+
+class Foil(object):
+    def __init__(self, peak_area, peak_error, activity, t_c)
+        self.peak = peak_area
+        self.peak_error = peak_error
+        self.activity = activity
+        self.t_c = counting_time
+
+
+data = {}
+data['al1'] = Foil(115.3, 14.62, -1, 3600)
+
