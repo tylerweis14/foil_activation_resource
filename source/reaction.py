afc5690750d48f3af427dcd6e9317e319d772d02
John Boyington <mjcb@ksu.edu>
2018-07-11 18:26:25 -0500

    added built-in roi calculation

--- source/reaction.py
+++ source/reaction.py
@@ -3,6 +3,7 @@ Houses the reaction object.
 '''
 from scipy.interpolate import interp1d
 import numpy as np
+from triga_spectrum import triga_spectrum
 
 foils = {}
 
@@ -28,9 +29,10 @@ class Reaction(object):
         self.func, self.region = self.extract()
         self.label, self.plotname = self.parse_text()
         self.classification = 'threshold' if self.region[0] > 1e4 else 'resonance'
+        self.roi = self.calc_roi()
 
     def extract(self):
-        xs_erg, xs_vals = np.loadtxt('xs/' + self.xs_string, delimiter=',', skiprows=1, unpack=True)
+        xs_erg, xs_vals = np.loadtxt('xs/' + self.xs_file, delimiter=',', skiprows=1, unpack=True)
         f = interp1d(xs_erg, xs_vals, bounds_error=False, fill_value=0)
         region = xs_erg[0], xs_erg[-1]
         return f, region
@@ -43,6 +45,28 @@ class Reaction(object):
         cd_s = '_Cd' if self.cd else ''
         plotname = '{}({}){}{}'.format(self.reactant, self.reaction, self.byproduct, cd_s)
         return label, plotname
+
+    def findroot(self, percentile, func):
+        l, r = self.region
+        c = np.geomspace(l, r, 3)[1]
+        while abs(func(c) - percentile) > 1e-12:
+            c = np.geomspace(l, r, 3)[1]
+            if percentile < func(c):
+                r = c
+            else:
+                l = c
+        return c
+
+    def calc_roi(self):
+        edges = np.geomspace(*self.region, 1e6)
+        midpoints = (edges[1:] + edges[:-1]) / 2
+        widths = (edges[1:] - edges[:-1])
+        rectangles = self.func(midpoints) * triga_spectrum(midpoints) * widths
+        rectangles = rectangles / np.sum(rectangles)
+        cdf_vals = np.cumsum(rectangles)
+        cdf = interp1d(midpoints, cdf_vals, fill_value=(rectangles[0], rectangles[-1]))
+        l, r = self.findroot(0.05, cdf), self.findroot(0.95, cdf)
+        return l, r
 
 foils['In'] = Reaction('In-115', 'n,gamma', 'In-116', False, 114.818, 7.31, '49-In-115(n,&gamma;).txt', 54*60, 1.0, 1293, 0.8, -1)
 foils['InCd'] = Reaction('In-115', 'n,gamma', 'In-116', True, 114.818, 7.31, '49-In-115(n,&gamma;).txt', 54*60, 1.0, 1293, 0.8, -1)
