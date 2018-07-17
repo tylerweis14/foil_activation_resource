42d980c526194d50b03b13e3f01d3b52611b43f2
John Boyington <mjcb@ksu.edu>
2018-07-17 12:26:34 -0500

    updated masses

--- source/theoretical_wisconsin.py
+++ source/theoretical_wisconsin.py
@@ -5,10 +5,10 @@ from theoretical import Theoretical
 
 
 irradiation_data = []
-irradiation_data.append(['indium', 'In', False, 1.1, 30, 3600*2, 60, 100])
-irradiation_data.append(['indium', 'InCd', True, 1.1, 30, 3600*2, 60, 100])
-irradiation_data.append(['gold', 'Au', False, 'estimate', 60, 3600*2, 120, 100])
-irradiation_data.append(['gold', 'AuCd', True, 'estimate', 60, 3600*2, 120, 100])
+irradiation_data.append(['indium', 'In', False, 2.1, 30, 3600*2, 60, 100])
+irradiation_data.append(['indium', 'InCd', True, 2.1, 30, 3600*2, 60, 100])
+irradiation_data.append(['gold', 'Au', False, 4.1, 60, 3600*2, 120, 100])
+irradiation_data.append(['gold', 'AuCd', True, 4.1, 60, 3600*2, 120, 100])
 irradiation_data.append(['molybdenum', 'Mo', False, 3.9, 3600, 3600*24*2, 600, 100])
 irradiation_data.append(['molybdenum', 'MoCd', True, 3.9, 3600, 3600*24*2, 600, 100])
 irradiation_data.append(['rhodium', 'Rh', False, 7.4, 3600, 3600*1, 1800, 100])
